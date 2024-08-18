d5cc6837ab898bb640e8db0b04f99c23de7c4d83
Inocêncio Cardoso <inocencio.cardoso@iatec.com>
2024-08-17 23:05:56 -0300

    feat: organização de diretórios

--- src/OutboxInboxPattern.AppHost/Program.cs
+++ src/OutboxInboxPattern.AppHost/Program.cs
@@ -1,5 +1,5 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.OutboxInboxPattern>("outboxinboxpattern");
+builder.AddProject<Projects.Outbox_Api>("outboxapi");
 
 await builder.Build().RunAsync();
